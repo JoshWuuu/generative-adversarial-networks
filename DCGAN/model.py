bce5a8e7eb427aedf0cbe106ea44dd9ca90e7f87
ChunLiang Wu <lucky0978218035@gmail.com>
2023-03-16 23:15:20 -0700

    Update model.py

--- DCGAN/model.py
+++ DCGAN/model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class DiscriminatorSimpleGAN(nn.Module):
@@ -26,46 +27,11 @@ class GeneratorSimpleGAN(nn.Module):
 
     def forward(self, x):
         return self.gen(x)
-    
-import torch
-import torch.nn as nn
-
-
-class DiscriminatorDCGAN(nn.Module):
-    def __init__(self, channels_img, features_d):
-        super(DiscriminatorDCGAN, self).__init__()
-        self.disc = nn.Sequential(
-            # input: N x channels_img x 64 x 64
-            nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            # _block(in_channels, out_channels, kernel_size, stride, padding)
-            self._block(features_d, features_d * 2, 4, 2, 1),
-            self._block(features_d * 2, features_d * 4, 4, 2, 1),
-            self._block(features_d * 4, features_d * 8, 4, 2, 1),
-            # After all _block img output is 4x4 (Conv2d below makes into 1x1)
-            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
-            nn.Sigmoid(),
-        )
-
-    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
-        return nn.Sequential(
-            nn.Conv2d(
-                in_channels,
-                out_channels,
-                kernel_size,
-                stride,
-                padding,
-                bias=False,
-            ),
-            # nn.BatchNorm2d(out_channels),
-            nn.LeakyReLU(0.2),
-        )
-
-    def forward(self, x):
-        return self.disc(x)
-
 
 class GeneratorDCGAN(nn.Module):
+    """
+    Generator network for DCGAN
+    """
     def __init__(self, channels_noise, channels_img, features_g):
         super(GeneratorDCGAN, self).__init__()
         self.net = nn.Sequential(
@@ -97,6 +63,42 @@ class GeneratorDCGAN(nn.Module):
 
     def forward(self, x):
         return self.net(x)
+    
+class DiscriminatorDCGAN(nn.Module):
+    """
+    Discriminator network for DCGAN
+    """
+    def __init__(self, channels_img, features_d):
+        super(DiscriminatorDCGAN, self).__init__()
+        self.disc = nn.Sequential(
+            # input: N x channels_img x 64 x 64
+            nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2),
+            # _block(in_channels, out_channels, kernel_size, stride, padding)
+            self._block(features_d, features_d * 2, 4, 2, 1),
+            self._block(features_d * 2, features_d * 4, 4, 2, 1),
+            self._block(features_d * 4, features_d * 8, 4, 2, 1),
+            # After all _block img output is 4x4 (Conv2d below makes into 1x1)
+            nn.Conv2d(features_d * 8, 1, kernel_size=4, stride=2, padding=0),
+            nn.Sigmoid(),
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            # nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2),
+        )
+
+    def forward(self, x):
+        return self.disc(x)
 
 class DiscriminatorWGAN(nn.Module):
     def __init__(self, channels_img, features_d):
@@ -167,7 +169,12 @@ class GeneratorWGAN(nn.Module):
         return self.net(x)
 
 def initialize_weights(model):
-    # Initializes weights according to the DCGAN paper
+    """
+    Initializes weights according to the DCGAN paper
+
+    Inputs:
+    - model: A PyTorch module containing the weights to be initialized
+    """
     for m in model.modules():
         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
             nn.init.normal_(m.weight.data, 0.0, 0.02)
@@ -176,12 +183,14 @@ def initialize_weights(model):
 def test():
     N, in_channels, H, W = 8, 3, 64, 64
     noise_dim = 100
-    x = torch.randn((N, in_channels, H, W))
-    disc = Discriminator(in_channels, 8)
-    assert disc(x).shape == (N, 1, 1, 1), "Discriminator test failed"
-    gen = Generator(noise_dim, in_channels, 8)
+
+    gen = GeneratorDCGAN(noise_dim, in_channels, 8)
     z = torch.randn((N, noise_dim, 1, 1))
     assert gen(z).shape == (N, in_channels, H, W), "Generator test failed"
+
+    x = torch.randn((N, in_channels, H, W))
+    disc = DiscriminatorDCGAN(in_channels, 8)
+    assert disc(x).shape == (N, 1, 1, 1), "Discriminator test failed"
     print("Success, tests passed!")
 
 
