d2f0509dace9494f205e0dbf2cab6270c1aef6bf
ChunLiang Wu <lucky0978218035@gmail.com>
2023-03-16 22:49:34 -0700

    Update main.py

--- CycleGAN/main.py
+++ CycleGAN/main.py
@@ -1,63 +1,55 @@
 import torch
 from dataset import HorseZebraDataset
-import sys
 from utils import save_checkpoint, load_checkpoint
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.optim as optim
 import config
-from tqdm import tqdm
-from torchvision.utils import save_image
-from discriminator_model import Discriminator
-from generator_model import Generator
+from model import *
+from train import train_fn
 
 def main():
-    disc_H = Discriminator(in_channels=3).to(config.DEVICE)
-    disc_Z = Discriminator(in_channels=3).to(config.DEVICE)
-    gen_Z = Generator(img_channels=3, num_residuals=9).to(config.DEVICE)
-    gen_H = Generator(img_channels=3, num_residuals=9).to(config.DEVICE)
+    # first gan 
+    gen_Z = GeneratorCycleGAN(img_channels=3, num_residuals=9).to(config.DEVICE)
+    disc_Z = DiscriminatorCycleGAN(in_channels=3).to(config.DEVICE)
+
+    # second gan
+    gen_H = GeneratorCycleGAN(img_channels=3, num_residuals=9).to(config.DEVICE)
+    disc_H = DiscriminatorCycleGAN(in_channels=3).to(config.DEVICE)
+    
+    # optimimzer for discriminator, concat the two discriminators
     opt_disc = optim.Adam(
         list(disc_H.parameters()) + list(disc_Z.parameters()),
         lr=config.LEARNING_RATE,
         betas=(0.5, 0.999),
-        )
+    )
 
     opt_gen = optim.Adam(
         list(gen_Z.parameters()) + list(gen_H.parameters()),
         lr=config.LEARNING_RATE,
         betas=(0.5, 0.999),
-        )
+    )
 
+    # loss function for cycle loss
     L1 = nn.L1Loss()
+    # loss function for adversarial loss
     mse = nn.MSELoss()
 
     if config.LOAD_MODEL:
         load_checkpoint(
-            config.CHECKPOINT_GEN_H,
-            gen_H,
-            opt_gen,
-            config.LEARNING_RATE,
+            config.CHECKPOINT_GEN_H, gen_H, opt_gen, config.LEARNING_RATE,
         )
         load_checkpoint(
-            config.CHECKPOINT_GEN_Z,
-            gen_Z,
-            opt_gen,
-            config.LEARNING_RATE,
+            config.CHECKPOINT_GEN_Z, gen_Z, opt_gen, config.LEARNING_RATE,
         )
         load_checkpoint(
-            config.CHECKPOINT_CRITIC_H,
-            disc_H,
-            opt_disc,
-            config.LEARNING_RATE,
+            config.CHECKPOINT_CRITIC_H, disc_H, opt_disc, config.LEARNING_RATE,
         )
         load_checkpoint(
-            config.CHECKPOINT_CRITIC_Z,
-            disc_Z,
-            opt_disc,
-            config.LEARNING_RATE,
+            config.CHECKPOINT_CRITIC_Z, disc_Z, opt_disc, config.LEARNING_RATE,
         )
 
-    dataset = HorseZebraDataset(
+    train_dataset = HorseZebraDataset(
         root_horse=config.TRAIN_DIR + "/horses",
         root_zebra=config.TRAIN_DIR + "/zebras",
         transform=config.transforms,
@@ -74,7 +66,7 @@ def main():
         pin_memory=True,
     )
     loader = DataLoader(
-        dataset,
+        train_dataset,
         batch_size=config.BATCH_SIZE,
         shuffle=True,
         num_workers=config.NUM_WORKERS,
@@ -85,26 +77,14 @@ def main():
 
     for epoch in range(config.NUM_EPOCHS):
         train_fn(
-            disc_H,
-            disc_Z,
-            gen_Z,
-            gen_H,
-            loader,
-            opt_disc,
-            opt_gen,
-            L1,
-            mse,
-            d_scaler,
-            g_scaler,
+            gen_Z, disc_Z, gen_H, disc_H, loader, opt_disc, opt_gen, L1, mse, d_scaler, g_scaler,
         )
 
         if config.SAVE_MODEL:
             save_checkpoint(gen_H, opt_gen, filename=config.CHECKPOINT_GEN_H)
             save_checkpoint(gen_Z, opt_gen, filename=config.CHECKPOINT_GEN_Z)
-            save_checkpoint(disc_H, opt_disc,
-                            filename=config.CHECKPOINT_CRITIC_H)
-            save_checkpoint(disc_Z, opt_disc,
-                            filename=config.CHECKPOINT_CRITIC_Z)
+            save_checkpoint(disc_H, opt_disc, filename=config.CHECKPOINT_CRITIC_H)
+            save_checkpoint(disc_Z, opt_disc, filename=config.CHECKPOINT_CRITIC_Z)
             
 if __name__ == "__main__":
     main()
